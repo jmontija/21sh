028cc60cf41135f74f9d14ec8724f67fb9679bff
jmontija <julien.montijano@gmail.com>
2016-05-04 14:28:56 +0200

    update

--- srcs/redirections.c
+++ srcs/redirections.c
@@ -3,19 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmontija <jmontija@student.42.fr>          +#+  +:+       +#+        */
+/*   By: julio <julio@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 02:03:59 by julio             #+#    #+#             */
-/*   Updated: 2016/05/03 19:23:59 by jmontija         ###   ########.fr       */
+/*   Updated: 2016/05/04 03:24:19 by julio            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
 /*
-	l'arg suivant la redirection est forcement un fichier
+	<> file -> make file
+	cmd (> < >> <<) file [file1 file2 ...]options_cmd
+	traité de gauche a droite
+		-> cmd < file2 <file1 > file4 < file3  >file5
+		-> cmd get info from file2 file1 file3 and redirect it to file4 file5 etc .. till next_cmd
 */
 
+/*
+	charspec = function(mychar)
+	{
+		int idx
+		char_str = "< > |"
+		char_tab = strsplit(char_str)
+		
+		while (chartab)
+				if (char_tab[str][0] == 'mychar')
+					return(str);
+		return (-1);
+	}
+	ft_teaser = function(grp, cmd_line)
+	{
+		while (cmd_line)
+			while(str)
+				if (idx = charspec(char) >= 0)
+					break
+		pointeursurfonction(idx, &fd);
+		parse(fd, grp)
+	}
+
+*/
 char	**create_redirection(char **cmd_line, int idx, int action)
 {
 	int	fd;
@@ -26,7 +53,7 @@ char	**create_redirection(char **cmd_line, int idx, int action)
 		return (NULL);
 	}
 	fd = open(cmd_line[idx + 2], O_WRONLY | action | O_CREAT, 0644);
-	dup2(fd, STDOUT_FILENO); // penser a reset le shell si cat ou tout autre fichier utilsant l'entree standard
+	dup2(fd, STDOUT_FILENO); // penser a reset le shell si cat ou autre fichier utilsant l'entree standard
 	close(fd);
 	while (cmd_line[++idx])
 		REMOVE(&cmd_line[idx]);
